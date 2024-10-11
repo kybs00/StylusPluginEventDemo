e2daea38406997ceed350c1d1c679d09c10ef22d
kybs00 <1016724958@qq.com>
2024-10-11 10:13:46 +0800

    添加项目文件。

---
+++ StylusPluginEventDemo/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Input.StylusPlugIns;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StylusPluginEventDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            StylusPlugIns.Add(new TestStylusPlugIn(Thread.CurrentThread.ManagedThreadId));
+        }
+    }
+    class TestStylusPlugIn : StylusPlugIn
+    {
+        private readonly int _uiThreadId;
+
+        public TestStylusPlugIn(int uiThreadId)
+        {
+            _uiThreadId = uiThreadId;
+        }
+
+        protected override void OnStylusMove(RawStylusInput rawStylusInput)
+        {
+            if (Thread.CurrentThread.ManagedThreadId == _uiThreadId)
+            {
+                return;
+            }
+            Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss.fff")}:StylusPluginOnStylusMove{rawStylusInput.GetStylusPoints().Count}个点");
+            base.OnStylusMove(rawStylusInput);
+        }
+    }
+}
